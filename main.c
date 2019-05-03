af2de97a18b557e6342b56c071d44fb42e3a02c2
xuz <uestcxz@163.com>
2019-05-03 22:46:30 +0800

    Version1.1.0
    Author:刘旭芝
    Date:2019.04.23
    更新日志:学习通过单向链表尾部方式插入新的节点(尾插法)

--- main.c
+++ main.c
@@ -1,40 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+//链表尾部插入节点
+/*
+ *          头节点(pHead)      尾节点(pTail)     插入的节点(pNode)
+ *          **************      **************     **************
+ *          *下一节点地址*      *下一节点地址*     *下一节点地址*
+ *          **************      **************     **************
+*/
 
-//定义一个节点
+
+
+
 struct Node1
 {
-    char name[30];  //存放当前节点的姓名
-    struct Node1 *next; //用于存放下一个节点的地址
+    char name[30];
+    struct Node1 *next; //存放下一节点地址
 };
 
 typedef struct Node1 Node;
 
 int main()
 {
-   //定义三个节点
-   Node stu1;
-   Node stu2;
-   Node stu3;
+    //一个指向头节点，用于遍历，一个指向尾部，用于插入新节点
+    Node *pHead = NULL;
+    Node *pTail = NULL;
 
-   //节点数据赋值
-   strcpy(stu1.name,"学生A");
-   strcpy(stu2.name,"学生B");
-   strcpy(stu3.name,"学生C");
+    while(1)
+    {
+        printf("请输入学生姓名，q(退出),p(打印)：\n");
+        char name[30];
+        scanf("%s",name);
 
-   //stu1作为头节点，stu3作为尾节点
-   stu1.next = &stu2;
-   stu2.next = &stu3;
-   stu3.next = NULL;
+        if(strcmp(name,"q")== 0)  //退出系统
+        {
+            printf("退出系统\n");
+            break;
+        }
+        if(strcmp(name,"p")==0) //打印
+        {
+            Node *pNode = pHead;
+            while(pNode !=NULL)//遍历链表
+            {
+                printf("%s   ",pNode->name);
+                pNode = pNode->next;
+            }
+            printf("\n");
+        }
+        else    //添加学生
+        {
+            Node *pNode =(Node *)malloc(sizeof(Node));
+            strcpy(pNode->name,name);
+            pNode->next = NULL; //默认将其作为尾部节点
 
-   //以上步骤已构建完成一个简单的单向链表
-   Node *pNode = &stu1;
-   while(pNode != NULL)
-   {
-       printf("姓名是:%s\n",pNode->name);
-       pNode = pNode ->next;
-   }
-   printf("\n");
+            //把节点放入链表中
+            /*  判定是否为头节点,因为前面建立的链表是空链表
+                第一次插入的节点就是头节点也是尾节点
+
+            */
+            if(pHead == NULL)
+            {
+                pHead = pNode;
+                pTail = pNode;
+            }
+            else
+            {
+                /*  由于采用的是尾部节点插入法，即需要替换掉初始化定义的尾部节点
+                    让插入的节点pNode作为尾部节点
+                */
+                pTail ->next = pNode;   //让尾部节点的下一个指向地址指向插入的节点
+                pTail = pNode;      //让插入的节点作为当前尾部节点
+            }
+
+        }
+    }
 
 }
