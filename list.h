809e354eb7302c04ea36a200aff7f6f64766bf53
xuz <uestcxz@163.com>
2019-05-06 17:54:30 +0800

    Version1.7.0
    Author:刘旭芝
    Date:2019.05.06
    更新日志:学习单向循环链表

--- list.h
+++ list.h
@@ -5,16 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-//向链表的中部插入节点
-typedef struct Node1
-{
-    char name[30];
-    struct Node1 *next;
-}Node;
 
-Node *CreateList(int N);
-void PrintList(Node *pHead);
-Node *DeleteList(Node *phead,const char *name);
 
 
 
--- main.c
+++ main.c
@@ -4,24 +4,51 @@
 #include "list.h"
 
 /*
- * Author: 螒蠀螤蟻桅楼
- * Date: 2019.05.04
- * 鍒犻櫎閾捐〃涓儴鑺傜偣
+ * Author: 刘旭芝
+ * Date: 2019.05.06
+ * 初步接触单向循环链表
 */
 
+
+//定义一个节点
+typedef struct Node1
+{
+    char name[30];
+    struct Node1 *next;
+}Node;
 
 
 int main()
 {
-    Node *p;
-    p = CreateList(1);
-    PrintList(p);
+    //建立三个节点
+    Node Stu1,Stu2,Stu3;
 
-    p = DeleteList(p,"瀛︾敓1");
+    //建立一个简单的单向循环链表
+    Stu1.next = &Stu2;
+    Stu2.next = &Stu3;
+    Stu3.next = &Stu1;
 
-    PrintList(p);
+    //给链表成员赋值
+    strcpy(Stu1.name,"学生1");
+    strcpy(Stu2.name,"学生2");
+    strcpy(Stu3.name,"学生3");
 
-    p = DeleteList(p,"瀛︾敓1");
-
-    PrintList(p);
+    //打印链表,不能通过判定最后一个next指向为NULL来判定
+     Node *pHead = &Stu1;
+     Node *pNode = pHead;
+     do
+     {
+         if(pNode != NULL)
+         {
+             printf("%s   ",pNode ->name);
+         pNode = pNode ->next;
+         }
+         else
+         {
+             printf("这是一条空链表\n");
+         }
+     }
+     while(pNode != pHead);
+     printf("\n");
 }
+
