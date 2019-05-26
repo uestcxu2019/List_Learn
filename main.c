43898005ba29b9aa812d28b879594a0232857caf
xuz <uestcxz@163.com>
2019-05-26 18:48:10 +0800

    Version2.0.0
    Author:刘旭芝
    Date:2019.05.26
    更新日志: 初步接触双向链表，构建一个最简单的双向链表

--- main.c
+++ main.c
@@ -5,12 +5,8 @@
 
 /*************************************************************
  * Author: 刘旭芝
- * Date: 2019.05.16
- * 删除单向循环链表头部节点
- *          步骤:
- *              1.遍历链表，找到最后一个节点
- *              2.删除头部节点(如果是malloc内存，要free)
- *              3.头指针指向新头节点，将尾节点的next指向新头节点
+ * Date: 2019.05.26
+ * 初步接触双向链表，构建一条最简单的双向链表，并且正反打印信息
 *************************************************************/
 
 
@@ -18,92 +14,48 @@
 typedef struct Node1
 {
     char name[30];
+    struct Node1 *prev;
     struct Node1 *next;
 }Node;
 
-//链表创建函数,传入参数为链表节点个数
-Node *CreateList(int n)
-{
-    Node *phead = NULL;
-    Node *pTail = NULL;
-    int i = 0;
-    for(i = 0;i < n;i++)
-    {
-        Node *pNode = (Node *)malloc(sizeof(Node));
-        sprintf(pNode->name,"学生%d",i+1);
-
-        if(phead == NULL)
-        {
-            phead = pTail = pNode;
-            pNode ->next = phead;
-        }
-        else
-        {
-            pTail ->next = pNode;
-            pTail = pNode;
-            pTail ->next = phead;
-        }
-
-    }
-    return phead;
-}
-
-
-//打印链表
-Node *PrintList(Node *phead)
-{
-    Node *pTemp = phead;
-    do
-    {
-        if(pTemp != NULL)
-        {
-            printf("%s   ",pTemp ->name);
-            pTemp = pTemp ->next;
-        }
-    }
-    while(pTemp != phead);
-    printf("\n");
-    return phead;
-}
-
-
-//删除链表头部节点函数，传入参数为链表头节点指针
-Node *DeleteList(Node *phead)
-{
-    Node *pTemp = phead;
-    //链表只有一个节点的情况
-    if(phead ->next == phead)
-    {
-        free(phead);
-        printf("唯一节点已删除,当前链表为空链表\n");
-        return NULL;
-    }
-
-    //遍历链表，找到尾部节点
-    while(pTemp ->next != phead)
-    {
-        pTemp = pTemp ->next;
-    }
-    printf("找到尾节点是:   %s\n",pTemp->name);
-
-    //将原链表的第二个节点作为新的节点
-    phead = phead ->next;
-
-    //删除头部节点(尾节点指向的下一个节点即为原头节点)
-    free(pTemp ->next);
-
-    //尾节点的next指向新的头节点
-    pTemp ->next = phead;
-
-    return phead;
-}
-
 int main()
 {
-    Node *p = NULL;
-    p = CreateList(1);
-    p = PrintList(p);
-    p = DeleteList(p);
-    p = PrintList(p);
+    //创建三个节点
+    Node stu1,stu2,stu3;
+
+    //节点赋值
+    strcpy(stu1.name,"学生A");
+    strcpy(stu2.name,"学生B");
+    strcpy(stu3.name,"学生C");
+
+    //串联成一个简单的双向链表
+    stu1.prev = NULL;
+    stu1.next = &stu2;
+
+    stu2.prev = &stu1;
+    stu2.next = &stu3;
+
+    stu3.prev = &stu2;
+    stu3.next = NULL;
+
+
+    //正向打印链表
+    Node *phead = &stu1;
+    while(phead)
+    {
+        printf("%s  ",phead ->name);
+        phead = phead ->next;
+    }
+    printf("\n");
+
+    //反向打印链表
+    Node *pTail = &stu3;
+    while(pTail)
+    {
+       printf("%s  ",pTail ->name);
+       pTail = pTail ->prev;
+    }
+
+    return 0;
 }
 
